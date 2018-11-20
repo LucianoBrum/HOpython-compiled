133451da962801870fccb54cbf1c38fdcf747dc7
ilcarbo <40764385+ilcarbo@users.noreply.github.com>
2018-11-19 21:46:34 -0300

    documentar un poco

--- src/libtest.py
+++ src/libtest.py
@@ -1,6 +1,8 @@
 import ctypes as C
 mylib = C.CDLL('./libmylib.so')
 
+# add_float
+print("add_float: ")
 mylib.add_float.restype = C.c_float
 mylib.add_float.argtypes = [C.c_float, C.c_float]
 
@@ -9,6 +11,8 @@ float2 = float(input("Enter another number:"))
 
 print(mylib.add_float(float1, float2))
 
+# add_int
+print("add_int: ")
 mylib.add_int.restype = C.c_int
 mylib.add_int.argtypes = [C.c_int, C.c_int]
 
@@ -17,6 +21,8 @@ int2 = int(input("Enter another number:"))
 
 print(mylib.add_int(int1, int2))
 
+# add_float_ref
+print("add_float_ref: ")
 cinco_float = C.c_float(5)
 seis_float = C.c_float(6)
 res_float = C.c_float()
@@ -29,11 +35,15 @@ mylib.add_float_ref(C.byref(cinco_float),
                     C.byref(res_float))
 print(res_float.value)
 
+# add_int_ref
+print("add_int_ref: ")
 mylib.add_int_ref(C.byref(cinco_int),
                   C.byref(seis_int),
                   C.byref(res_int))
 print(res_int.value)
 
+# add_int_array
+print("add_int_array: ")
 in1 = (C.c_int * 3) (2, 4, -2)
 in2 = (C.c_int * 3) (3, -1, -2)
 out = (C.c_int * 3) ()
@@ -43,6 +53,8 @@ mylib.add_int_array(C.byref(in1),
                     3)
 print(out[0], out[1], out[2])
 
+# dot_product
+print("dot_product: ")
 flin1 = (C.c_float * 3) (2.0, 1.5, 3.2)
 flin2 = (C.c_float * 3) (1.5, -3.0, 0.5)
 flout = C.c_float()
